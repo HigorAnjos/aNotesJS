bdf2ecf3ed12abb326445313ece453549a9d557f
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-01-20 13:50:58 -0300

    impl: metodos do app

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,35 @@
 import NotesView from "./NotesView.js";
+import NotesAPI from "./NotesAPI.js";
 
 export default class App {
   constructor(root) {
     this.notes = [];
     this.activeNote = null;
     this.view = new NotesView(root, this._hendlers());
+
+    this._refreshNotes();
+  }
+
+  _refreshNotes ()  {
+    const notes = NotesAPI.getAllNotes();
+
+    this._setNotes(notes);
+
+    if (notes.length > 0) {
+      this._setActiveNote(notes[0]);
+    }
+
+  }
+
+  _setNotes(notes) {
+    this.notes = notes;
+    this.view.updateNoteList(notes)
+    this.view.updateNotePreviewVisibility(notes.length > 0);
+  }
+
+  _setActiveNote(note) {
+    this.activeNote = note;
+    this.view.updateActiveNote(note);
   }
 
   _hendlers() {
