adf23aa099c6a360211ab83437fba60bbd3e93ff
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-01-20 11:17:33 -0300

    _createListItemHTML

--- public/js/NotesView.js
+++ public/js/NotesView.js
@@ -1,8 +1,9 @@
 export default class NotesView {
-  constructor(root, { onNoteSelect, onNoteAdd, onNoteDelete } = {}) {
+  constructor(root, { onNoteSelect, onNoteAdd, onNoteEdit ,onNoteDelete } = {}) {
     this.root =  root;
     this.onNoteSelect = onNoteSelect;
     this.onNoteAdd = onNoteAdd;
+    this.onNoteEdit = onNoteEdit;
     this.onNoteDelete = onNoteDelete;
     this.root.innerHTML = `
         <div class="notes__sidebar">
@@ -23,7 +24,36 @@ export default class NotesView {
 
     btnAddNote.addEventListener("click", () => {
       this.onNoteAdd();
-    })
+    });
+
+    [inpTitle, inpBody].forEach(inputField => {
+      inputField.addEventListener("blur", () => {
+        const updatedTitle = inpTitle.value.trim();
+        const updatedBody = inpBody.value.trim();
+
+        this.onNoteEdit(updatedTitle, updatedBody)
+      });
+    });
+
+    // TODO: hide the note preview by default
 
   }
+
+  _createListItemHTML(id, title, body, updated) {
+    const MAX_BODY_LENGTH = 60;
+
+    return `
+      <div class="notes__list-item" data-note-id="${id}">
+        <div class="notes__smal-title">${title}</div>
+        <div class="notes__smal-body">
+          ${body.substring(0, MAX_BODY_LENGTH)}
+          ${body.length > MAX_BODY_LENGTH ? "..." : ""}
+        </div>
+        <div class="notes__smal-updated">
+          ${updated.toLocalString(undefined, { dateStyle: "full", timeStyle: "short"})}
+        </div>
+      </div>
+    `;
+  }
+
 }
--- public/js/main.js
+++ public/js/main.js
@@ -4,5 +4,9 @@ const app = document.getElementById("app");
 const view = new NotesView(app, {
   onNoteAdd() {
     console.log(" Funcao para o addNote")
+  },
+  onNoteEdit(newTitle, newBody) {
+    console.log(newTitle)
+    console.log(newBody)
   }
 });
